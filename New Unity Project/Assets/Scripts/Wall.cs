4109ac48396c840b077d45be89b2893b5898cf84
antonioalarcon32 <antonioalarcn32@gmail.com>
2020-04-25 20:49:49 +0200

    Falta adaptar a android

---
+++ New Unity Project/Assets/Scripts/Wall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite;                 
+    public int hp = 3;
+    public AudioClip chop1;
+    public AudioClip chop2;
+
+
+    private SpriteRenderer spriteRenderer; 
+
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    public void DamageWall(int loss)
+    {
+        SoundManager.instance.RandomizeSfx(chop1, chop2);
+        spriteRenderer.sprite = dmgSprite;
+        hp -= loss;
+        if (hp <= 0)
+            gameObject.SetActive(false);
+    }
+}
